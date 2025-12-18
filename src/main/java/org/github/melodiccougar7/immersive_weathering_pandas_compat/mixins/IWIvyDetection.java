6499f3bb5691d265ebaac198362f1febdc9a44de
MelodicCougar7 <melodiccougar7@gmail.com>
2025-12-18 15:42:14 -0500

    initial commit patch

--- src/main/java/org/github/melodiccougar7/immersive_weathering_pandas_compat/mixins/IWIvyDetection.java
+++ src/main/java/org/github/melodiccougar7/immersive_weathering_pandas_compat/mixins/IWIvyDetection.java
@@ -1,13 +1,19 @@
 package org.github.melodiccougar7.immersive_weathering_pandas_compat.mixins;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.ordana.immersive_weathering.reg.ModBlocks;
+import me.pandamods.fallingtrees.trees.GenericTree;
+
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-public class IWVineDetection {
+@Mixin(value = GenericTree, remap = false)
+public class IWIvyDetection {
     @ModifyExpressionValue(
             method = "targetMethod",
-            at = @At(value = "INVOKE", target = "Ltarget/Class;shouldFly()Z")
+            at = @At(value = "INVOKE", target = "placeholder") //"Lblusunrize/immersiveengineering/api/crafting/StackWithChance;chance()F"
     )
-    private boolean includeIWVines(boolean original) {
-        return original && YourMod.isFlyingAllowed();
+    private boolean includeIWIvy(boolean original) {
+        return original && currentState.is(ModBlocks.IVY);
     }
 }
